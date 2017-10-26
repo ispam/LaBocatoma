03088522d5b5e5b2bbaf00a18f38afab814ebc29
Diego <diegoug@gmail.com>
2017-10-26 17:58:52 -0500

    Google sign in created

--- app/src/main/java/destinum/tech/labocatoma/Activities/HomeActivity.java
+++ app/src/main/java/destinum/tech/labocatoma/Activities/HomeActivity.java
@@ -1,13 +1,15 @@
-package destinum.tech.labocatoma;
+package destinum.tech.labocatoma.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LandingActivity extends AppCompatActivity {
+import destinum.tech.labocatoma.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/destinum/tech/labocatoma/Activities/LandingActivity.java
@@ -0,0 +1,28 @@
+package destinum.tech.labocatoma.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import destinum.tech.labocatoma.R;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(LandingActivity.this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+
+    }
+}
